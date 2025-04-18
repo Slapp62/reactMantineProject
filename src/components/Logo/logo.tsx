ce0961ba469a491156ce6244f0872be91d600832
Simcha <slapp62@gmail.com>
2025-04-18 14:13:27 +0300

    icon fix

--- src/components/Logo/logo.tsx
+++ src/components/Logo/logo.tsx
@@ -1,10 +1,11 @@
 import { Group,Image,Text } from "@mantine/core";
+import flavicon from '/src/flavicon.png'
 
 export function Logo() {
     return (
         <Group>
               <Image
-              src="/src/flavicon.png"
+              src={flavicon}
               alt="Logo"
               style={{ width: '30px', height: '30px'}}/>
             
