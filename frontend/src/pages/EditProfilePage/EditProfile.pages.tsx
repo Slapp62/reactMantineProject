ca984639fcd7a41a70e784751c10b3a7753cd181
Simcha <slapp62@gmail.com>
2025-07-31 23:37:02 +0300

    comment out edit profil and admin controls for now

--- frontend/src/pages/EditProfilePage/EditProfile.pages.tsx
+++ frontend/src/pages/EditProfilePage/EditProfile.pages.tsx
@@ -1,207 +1,207 @@
-import { IconPhone } from '@tabler/icons-react';
-import { Button, Fieldset, Flex, Image, Text, TextInput, Title } from '@mantine/core';
-import { DeleteUserModal } from '@/components/Modals/DeleteUserModal';
-import { useEditProfile } from './useEditProfile';
+// import { IconPhone } from '@tabler/icons-react';
+// import { Button, Fieldset, Flex, Image, Text, TextInput, Title } from '@mantine/core';
+// import { DeleteUserModal } from '@/components/Modals/DeleteUserModal';
+// import { useEditProfile } from './useEditProfile';
 
-export function EditProfile() {
-  const {
-    isSubmitting,
-    isAdminView,
-    userData,
-    register,
-    handleSubmit,
-    onSubmit,
-    trigger,
-    errors,
-    isDirty,
-    isValid,
-    isDisabled,
-    setDisabled,
-    updateBusinessStatus,
-    isMobile,
-    opened,
-    open,
-    close,
-    deleteUser,
-  } = useEditProfile();
+// export function EditProfile() {
+//   const {
+//     isSubmitting,
+//     isAdminView,
+//     userData,
+//     register,
+//     handleSubmit,
+//     onSubmit,
+//     trigger,
+//     errors,
+//     isDirty,
+//     isValid,
+//     isDisabled,
+//     setDisabled,
+//     updateBusinessStatus,
+//     isMobile,
+//     opened,
+//     open,
+//     close,
+//     deleteUser,
+//   } = useEditProfile();
 
-  return (
-    <>
-      <Flex mt={20} direction="column" align="center" gap={20}>
-        <Title>Edit Profile</Title>
+//   return (
+//     <>
+//       <Flex mt={20} direction="column" align="center" gap={20}>
+//         <Title>Edit Profile</Title>
 
-        <Flex justify="center" direction="column" style={{ width: isMobile ? '90%' : '50%' }}>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Flex gap={10} direction="column" m="auto">
-              <Fieldset legend="Name">
-                <TextInput
-                  label="First"
-                  disabled={isDisabled}
-                  {...register('name.first')}
-                  error={errors.name?.first?.message}
-                />
-                <TextInput
-                  label="Middle"
-                  disabled={isDisabled}
-                  {...register('name.middle')}
-                  error={errors.name?.middle?.message}
-                />
-                <TextInput
-                  label="Last"
-                  disabled={isDisabled}
-                  {...register('name.last')}
-                  error={errors.name?.last?.message}
-                />
-              </Fieldset>
+//         <Flex justify="center" direction="column" style={{ width: isMobile ? '90%' : '50%' }}>
+//           <form onSubmit={handleSubmit(onSubmit)}>
+//             <Flex gap={10} direction="column" m="auto">
+//               <Fieldset legend="Name">
+//                 <TextInput
+//                   label="First"
+//                   disabled={isDisabled}
+//                   {...register('name.first')}
+//                   error={errors.name?.first?.message}
+//                 />
+//                 <TextInput
+//                   label="Middle"
+//                   disabled={isDisabled}
+//                   {...register('name.middle')}
+//                   error={errors.name?.middle?.message}
+//                 />
+//                 <TextInput
+//                   label="Last"
+//                   disabled={isDisabled}
+//                   {...register('name.last')}
+//                   error={errors.name?.last?.message}
+//                 />
+//               </Fieldset>
 
-              <Fieldset legend="Address">
-                <TextInput
-                  label="Country"
-                  disabled={isDisabled}
-                  {...register('address.country')}
-                  error={errors.address?.country?.message}
-                />
-                <TextInput
-                  label="State"
-                  disabled={isDisabled}
-                  {...register('address.state')}
-                  error={errors.address?.state?.message}
-                />
-                <TextInput
-                  label="City"
-                  disabled={isDisabled}
-                  {...register('address.city')}
-                  error={errors.address?.city?.message}
-                />
-                <TextInput
-                  label="Street"
-                  disabled={isDisabled}
-                  {...register('address.street')}
-                  error={errors.address?.street?.message}
-                />
-                <TextInput
-                  label="House Number"
-                  required
-                  disabled={isDisabled}
-                  {...register('address.houseNumber', {
-                    onChange: (e) => {
-                      e.target.value = e.target.value.replace(/\D/g, '');
-                    },
-                  })}
-                  error={errors.address?.houseNumber?.message}
-                />
-                <TextInput
-                  label="Zipcode"
-                  disabled={isDisabled}
-                  {...register('address.zip', {
-                    onChange: (e) => {
-                      e.target.value = e.target.value.replace(/\D/g, '');
-                    },
-                  })}
-                  error={errors.address?.zip?.message}
-                />
-              </Fieldset>
+//               <Fieldset legend="Address">
+//                 <TextInput
+//                   label="Country"
+//                   disabled={isDisabled}
+//                   {...register('address.country')}
+//                   error={errors.address?.country?.message}
+//                 />
+//                 <TextInput
+//                   label="State"
+//                   disabled={isDisabled}
+//                   {...register('address.state')}
+//                   error={errors.address?.state?.message}
+//                 />
+//                 <TextInput
+//                   label="City"
+//                   disabled={isDisabled}
+//                   {...register('address.city')}
+//                   error={errors.address?.city?.message}
+//                 />
+//                 <TextInput
+//                   label="Street"
+//                   disabled={isDisabled}
+//                   {...register('address.street')}
+//                   error={errors.address?.street?.message}
+//                 />
+//                 <TextInput
+//                   label="House Number"
+//                   required
+//                   disabled={isDisabled}
+//                   {...register('address.houseNumber', {
+//                     onChange: (e) => {
+//                       e.target.value = e.target.value.replace(/\D/g, '');
+//                     },
+//                   })}
+//                   error={errors.address?.houseNumber?.message}
+//                 />
+//                 <TextInput
+//                   label="Zipcode"
+//                   disabled={isDisabled}
+//                   {...register('address.zip', {
+//                     onChange: (e) => {
+//                       e.target.value = e.target.value.replace(/\D/g, '');
+//                     },
+//                   })}
+//                   error={errors.address?.zip?.message}
+//                 />
+//               </Fieldset>
 
-              <Fieldset legend="Contact">
-                <TextInput
-                  rightSection={<IconPhone />}
-                  label="Phone"
-                  disabled={isDisabled}
-                  required
-                  {...register('phone', {
-                    onChange: (e) => {
-                      e.target.value = e.target.value.replace(/[^\d-]/g, '');
-                    },
-                  })}
-                  error={errors.phone?.message}
-                />
-              </Fieldset>
+//               <Fieldset legend="Contact">
+//                 <TextInput
+//                   rightSection={<IconPhone />}
+//                   label="Phone"
+//                   disabled={isDisabled}
+//                   required
+//                   {...register('phone', {
+//                     onChange: (e) => {
+//                       e.target.value = e.target.value.replace(/[^\d-]/g, '');
+//                     },
+//                   })}
+//                   error={errors.phone?.message}
+//                 />
+//               </Fieldset>
 
-              <Fieldset legend="Image">
-                <Image src={userData?.image?.url} h={100} w={100} mt={10} m="auto" />
-                <TextInput
-                  label="New URL"
-                  disabled={isDisabled}
-                  {...register('image.url')}
-                  error={errors.image?.url?.message}
-                />
-                <TextInput
-                  label="New Image Alt"
-                  disabled={isDisabled}
-                  {...register('image.alt')}
-                  error={errors.image?.alt?.message}
-                />
-              </Fieldset>
+//               <Fieldset legend="Image">
+//                 <Image src={userData?.image?.url} h={100} w={100} mt={10} m="auto" />
+//                 <TextInput
+//                   label="New URL"
+//                   disabled={isDisabled}
+//                   {...register('image.url')}
+//                   error={errors.image?.url?.message}
+//                 />
+//                 <TextInput
+//                   label="New Image Alt"
+//                   disabled={isDisabled}
+//                   {...register('image.alt')}
+//                   error={errors.image?.alt?.message}
+//                 />
+//               </Fieldset>
 
-              <Fieldset legend="Change Account Type">
-                <Flex align="center" direction="column" justify="center" gap={10}>
-                  <Text>
-                    Account Type:{' '}
-                    {userData?.isAdmin ? (
-                      <strong>Admin User</strong>
-                    ) : userData?.isBusiness ? (
-                      <strong>Business User</strong>
-                    ) : (
-                      <strong>Regular User</strong>
-                    )}
-                  </Text>
+//               <Fieldset legend="Change Account Type">
+//                 <Flex align="center" direction="column" justify="center" gap={10}>
+//                   <Text>
+//                     Account Type:{' '}
+//                     {userData?.isAdmin ? (
+//                       <strong>Admin User</strong>
+//                     ) : userData?.isBusiness ? (
+//                       <strong>Business User</strong>
+//                     ) : (
+//                       <strong>Regular User</strong>
+//                     )}
+//                   </Text>
 
-                  {!userData?.isAdmin && (
-                    <Button
-                      size="xs"
-                      disabled={isDisabled}
-                      loading={isSubmitting}
-                      onClick={() => updateBusinessStatus()}
-                    >
-                      <Text fz="sm">Change</Text>
-                    </Button>
-                  )}
+//                   {!userData?.isAdmin && (
+//                     <Button
+//                       size="xs"
+//                       disabled={isDisabled}
+//                       loading={isSubmitting}
+//                       onClick={() => updateBusinessStatus()}
+//                     >
+//                       <Text fz="sm">Change</Text>
+//                     </Button>
+//                   )}
 
-                  {userData?.isAdmin && (
-                    <Text size="xs" c="red">
-                      Cannot change or delete an admin user
-                    </Text>
-                  )}
-                </Flex>
-              </Fieldset>
+//                   {userData?.isAdmin && (
+//                     <Text size="xs" c="red">
+//                       Cannot change or delete an admin user
+//                     </Text>
+//                   )}
+//                 </Flex>
+//               </Fieldset>
 
-              {userData?.isAdmin === false && isAdminView === false && (
-                <Fieldset legend="Delete Account">
-                  <Flex align="center" direction="column" gap={5}>
-                    <Text fw="bold" c="red">
-                      All data will be lost and you will be logged out.
-                    </Text>
-                    <Button color="red" disabled={isDisabled} onClick={open}>
-                      Delete Account
-                    </Button>
-                  </Flex>
-                </Fieldset>
-              )}
+//               {userData?.isAdmin === false && isAdminView === false && (
+//                 <Fieldset legend="Delete Account">
+//                   <Flex align="center" direction="column" gap={5}>
+//                     <Text fw="bold" c="red">
+//                       All data will be lost and you will be logged out.
+//                     </Text>
+//                     <Button color="red" disabled={isDisabled} onClick={open}>
+//                       Delete Account
+//                     </Button>
+//                   </Flex>
+//                 </Fieldset>
+//               )}
 
-              <Flex direction="column" gap={5} w="50%" mx="auto">
-                {isDisabled && (
-                  <Button
-                    onClick={() => {
-                      setDisabled(false);
-                      trigger();
-                    }}
-                  >
-                    {' '}
-                    Edit Profile
-                  </Button>
-                )}
+//               <Flex direction="column" gap={5} w="50%" mx="auto">
+//                 {isDisabled && (
+//                   <Button
+//                     onClick={() => {
+//                       setDisabled(false);
+//                       trigger();
+//                     }}
+//                   >
+//                     {' '}
+//                     Edit Profile
+//                   </Button>
+//                 )}
 
-                <Button disabled={!isValid || !isDirty} type="submit">
-                  {' '}
-                  Update Info
-                </Button>
-              </Flex>
-            </Flex>
-          </form>
-        </Flex>
-      </Flex>
+//                 <Button disabled={!isValid || !isDirty} type="submit">
+//                   {' '}
+//                   Update Info
+//                 </Button>
+//               </Flex>
+//             </Flex>
+//           </form>
+//         </Flex>
+//       </Flex>
 
-      <DeleteUserModal opened={opened} close={close} deleteUser={deleteUser} />
-    </>
-  );
-}
+//       <DeleteUserModal opened={opened} close={close} deleteUser={deleteUser} />
+//     </>
+//   );
+// }
--- frontend/src/pages/EditProfilePage/useEditProfile.ts
+++ frontend/src/pages/EditProfilePage/useEditProfile.ts
@@ -1,155 +1,156 @@
-import { useEffect, useState } from 'react';
-import { joiResolver } from '@hookform/resolvers/joi';
-import axios from 'axios';
-import { FieldValues, useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { useDisclosure, useMediaQuery } from '@mantine/hooks';
-import { RootState } from '@/store/store';
-import { clearUser, setUser, updateUser } from '@/store/authSlice';
-import { TUsers } from '@/Types';
-import { editProfileSchema } from '@/validationRules/editProfile.joi';
+// import { useEffect, useState } from 'react';
+// import { joiResolver } from '@hookform/resolvers/joi';
+// import axios from 'axios';
+// import { FieldValues, useForm } from 'react-hook-form';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useNavigate, useParams } from 'react-router-dom';
+// import { toast } from 'react-toastify';
+// import { useDisclosure, useMediaQuery } from '@mantine/hooks';
+// import { RootState } from '@/store/store';
+// import { clearUser, setUser, updateUser } from '@/store/authSlice';
+// import { TUser } from '@/Types';
+// import { editProfileSchema } from '@/validationRules/editProfile.joi';
+// import { useCurrentUser } from '@/utils/reduxHelperHooks';
 
-export const useEditProfile = () => {
-  const jumpTo = useNavigate();
-  const { id } = useParams();
-  const isMobile = useMediaQuery('(max-width: 700px)');
-  const [isDisabled, setDisabled] = useState(true);
-  const dispatch = useDispatch();
-  const [isSubmitting, setSubmitting] = useState(false);
-  const [opened, { open, close }] = useDisclosure(false);
+// export const useEditProfile = () => {
+//   const jumpTo = useNavigate();
+//   const { id } = useParams();
+//   const isMobile = useMediaQuery('(max-width: 700px)');
+//   const [isDisabled, setDisabled] = useState(true);
+//   const dispatch = useDispatch();
+//   const [isSubmitting, setSubmitting] = useState(false);
+//   const [opened, { open, close }] = useDisclosure(false);
 
-  const isAdminView = useSelector((state: RootState) => state.userSlice.isAdminView);
-  const currentUser = useSelector((state: RootState) => state.userSlice.user);
-  const allUsers = useSelector((state: RootState) => state.userSlice.allUsers);
-  const paramsUser = allUsers?.find((account) => account._id === id);
+//   const isAdminView = useSelector((state: RootState) => state.userSlice.isAdminView);
+//   const currentUser = useCurrentUser();
+//   const allUsers = useSelector((state: RootState) => state.userSlice.allUsers);
+//   const paramsUser = allUsers?.find((account) => account._id === id);
 
-  const userData = isAdminView ? paramsUser : currentUser;
+//   const userData = isAdminView ? paramsUser : currentUser;
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors, isValid, isDirty },
-    trigger,
-  } = useForm<TUsers>({
-    mode: 'all',
-    resolver: joiResolver(editProfileSchema),
-    defaultValues: userData ? cleanedUserData(userData) : {},
-  });
+//   const {
+//     register,
+//     handleSubmit,
+//     reset,
+//     formState: { errors, isValid, isDirty },
+//     trigger,
+//   } = useForm<TUser>({
+//     mode: 'all',
+//     resolver: joiResolver(editProfileSchema),
+//     defaultValues: userData ? cleanedUserData(userData) : {},
+//   });
 
-  useEffect(() => {
-    if (userData) {
-      const defaultUserValues = cleanedUserData(userData);
-      reset(defaultUserValues);
-    }
-  }, [reset, userData]);
+//   useEffect(() => {
+//     if (userData) {
+//       const defaultUserValues = cleanedUserData(userData);
+//       reset(defaultUserValues);
+//     }
+//   }, [reset, userData]);
 
-  const onSubmit = async (data: FieldValues) => {
-    if (!data.password) {
-      delete data.password;
-    }
-    data.address.houseNumber = Number(data.address.houseNumber);
-    data.address.zip = Number(data.address.zip);
-    try {
-      const response = await axios.put(
-        `https://monkfish-app-z9uza.ondigitalocean.app/bcard2/users/${userData?._id}`,
-        data
-      );
+//   const onSubmit = async (data: FieldValues) => {
+//     if (!data.password) {
+//       delete data.password;
+//     }
+//     data.address.houseNumber = Number(data.address.houseNumber);
+//     data.address.zip = Number(data.address.zip);
+//     try {
+//       const response = await axios.put(
+//         `https://monkfish-app-z9uza.ondigitalocean.app/bcard2/users/${userData?._id}`,
+//         data
+//       );
 
-      if (response.status === 200) {
-        const updatedUser = response.data;
+//       if (response.status === 200) {
+//         const updatedUser = response.data;
 
-        // if not admin view, update the current user information
-        if (!isAdminView) {
-          dispatch(setUser(updatedUser));
-        }
+//         // if not admin view, update the current user information
+//         if (!isAdminView) {
+//           dispatch(setUser(updatedUser));
+//         }
 
-        // if it is admin view, update the selected user information
-        if (isAdminView) {
-          dispatch(updateUser(updatedUser));
-        }
-        reset(cleanedUserData(updatedUser));
-        setDisabled(true);
-        toast.success('Profile Updated Successfully!', { position: `bottom-right` });
-      }
-    } catch (error: any) {
-      toast.error(`Update Failed! ${error.response.data}`, { position: `bottom-right` });
-    }
-  };
+//         // if it is admin view, update the selected user information
+//         if (isAdminView) {
+//           dispatch(updateUser(updatedUser));
+//         }
+//         reset(cleanedUserData(updatedUser));
+//         setDisabled(true);
+//         toast.success('Profile Updated Successfully!', { position: `bottom-right` });
+//       }
+//     } catch (error: any) {
+//       toast.error(`Update Failed! ${error.response.data}`, { position: `bottom-right` });
+//     }
+//   };
 
-  const updateBusinessStatus = async () => {
-    const token = localStorage.getItem('token') || sessionStorage.getItem('token');
-    axios.defaults.headers.common['x-auth-token'] = token;
-    try {
-      const response = await axios.patch(
-        `https://monkfish-app-z9uza.ondigitalocean.app/bcard2/users/${userData?._id}`
-      );
-      if (response.status === 200) {
-        const updatedUser = response.data;
-        setSubmitting(true);
-        setTimeout(() => {
-          // if not admin view, update the current user information
-          if (!isAdminView) {
-            dispatch(setUser(updatedUser));
-          }
-          // if it is admin view, update the selected user information
-          if (isAdminView) {
-            dispatch(updateUser(updatedUser));
-          }
-          toast.success('Account Status Updated');
-          setSubmitting(false);
-        }, 2000);
-      }
-    } catch (error: any) {
-      toast.error(`Account Status Update Failed! ${error.response.data}`, {
-        position: `bottom-right`,
-      });
-      if (error.response) {
-        toast.error(`Account Status Update Failed! ${error.response.data}`, {
-          position: `bottom-right`,
-        });
-      }
-    }
-  };
+//   const updateBusinessStatus = async () => {
+//     const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+//     axios.defaults.headers.common['x-auth-token'] = token;
+//     try {
+//       const response = await axios.patch(
+//         `https://monkfish-app-z9uza.ondigitalocean.app/bcard2/users/${userData?._id}`
+//       );
+//       if (response.status === 200) {
+//         const updatedUser = response.data;
+//         setSubmitting(true);
+//         setTimeout(() => {
+//           // if not admin view, update the current user information
+//           if (!isAdminView) {
+//             dispatch(setUser(updatedUser));
+//           }
+//           // if it is admin view, update the selected user information
+//           if (isAdminView) {
+//             dispatch(updateUser(updatedUser));
+//           }
+//           toast.success('Account Status Updated');
+//           setSubmitting(false);
+//         }, 2000);
+//       }
+//     } catch (error: any) {
+//       toast.error(`Account Status Update Failed! ${error.response.data}`, {
+//         position: `bottom-right`,
+//       });
+//       if (error.response) {
+//         toast.error(`Account Status Update Failed! ${error.response.data}`, {
+//           position: `bottom-right`,
+//         });
+//       }
+//     }
+//   };
 
-  const deleteUser = async () => {
-    const token = localStorage.getItem('token') || sessionStorage.getItem('token');
-    axios.defaults.headers.common['x-auth-token'] = token;
-    try {
-      const response = await axios.delete(
-        `https://monkfish-app-z9uza.ondigitalocean.app/bcard2/users/${userData?._id}`
-      );
-      if (response.status === 200) {
-        !isAdminView ? dispatch(clearUser()) : jumpTo('/admin');
-        toast.warning('Account Deleted.', { position: 'bottom-right' });
-      }
-    } catch (error: any) {
-      toast.error(`Account Deletion Failed! ${error.response.data}`, {
-        position: `bottom-right`,
-      });
-    }
-  };
+//   const deleteUser = async () => {
+//     const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+//     axios.defaults.headers.common['x-auth-token'] = token;
+//     try {
+//       const response = await axios.delete(
+//         `https://monkfish-app-z9uza.ondigitalocean.app/bcard2/users/${userData?._id}`
+//       );
+//       if (response.status === 200) {
+//         !isAdminView ? dispatch(clearUser()) : jumpTo('/admin');
+//         toast.warning('Account Deleted.', { position: 'bottom-right' });
+//       }
+//     } catch (error: any) {
+//       toast.error(`Account Deletion Failed! ${error.response.data}`, {
+//         position: `bottom-right`,
+//       });
+//     }
+//   };
 
-  return {
-    isSubmitting,
-    isAdminView,
-    userData,
-    register,
-    handleSubmit,
-    onSubmit,
-    trigger,
-    errors,
-    isDirty,
-    isValid,
-    isDisabled,
-    setDisabled,
-    updateBusinessStatus,
-    isMobile,
-    opened,
-    open,
-    close,
-    deleteUser,
-  };
-};
+//   return {
+//     isSubmitting,
+//     isAdminView,
+//     userData,
+//     register,
+//     handleSubmit,
+//     onSubmit,
+//     trigger,
+//     errors,
+//     isDirty,
+//     isValid,
+//     isDisabled,
+//     setDisabled,
+//     updateBusinessStatus,
+//     isMobile,
+//     opened,
+//     open,
+//     close,
+//     deleteUser,
+//   };
+// };
